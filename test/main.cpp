8c30d81e9a0a3acd9e6d0f9ca3460d9f0c4678b2
Daniel Boll <danielboll.academico@gmail.com>
2022-08-10 14:22:50 -0300

    feat(ci): testing the addition of a CI process
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

---
+++ test/main.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+
+TEST(Main, Main) { EXPECT_EQ(1, 1); }
---
+++ xmake.lua
@@ -0,0 +1,18 @@
+add_rules("mode.debug", "mode.release")
+set_languages("c++20")
+
+local libs = { "gtest" }
+
+add_includedirs("include")
+add_requires(table.unpack(libs))
+
+-- target("ci_lib")
+--   set_kind("static")
+--   add_files("source/*.cpp")
+--   add_packages(table.unpack(libs))
+
+target("ci_test")
+    set_kind("binary")
+    add_files("test/*.cpp")
+    add_packages(table.unpack(libs))
+    -- add_deps("ci_lib")
